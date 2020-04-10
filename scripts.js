f00fb122c347e9479526ad0e7b81fef21d56b7bb
Tinaye <tinayelmartin@gmail.com>
2020-04-10 15:42:41 +0200

    Fixed hamburger

--- scripts.js
+++ scripts.js
@@ -1,11 +1,19 @@
-$(function () {
+$(document).ready(function () {
   $(".hamburger-menu").on("click", function () {
-    $("toggle").toggleClass("open");
-    $("nav-link").toggleClass("open");
+    $(this).toggleClass("open");
+    $(".nav-list").toggleClass("open");
   });
 
   AOS.init({
     easing: escape,
     duration: 1000,
+  });
+});
+
+$(document).ready(function () {
+  $(".nav-item").on("click", function () {
+    $(".hamburger-menu").toggleClass("open");
+    $(this).toggleClass("open");
+    $(".nav-list").toggleClass("open");
   });
 });
